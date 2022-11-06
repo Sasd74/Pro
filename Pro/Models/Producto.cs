3ab584cccc48a059eed6376b5a68cfa0f3c7961e
Sasd74 <alonso052792@hotmail.com>
2022-11-06 14:14:19 -0600

    Agregar archivos de proyecto.

---
+++ Pro/Models/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pro.Models
+{
+    class Producto
+    {
+        public int Id { get; set; }
+        public string Descripciones { get; set; }
+        public double Costo { get; set; }
+        public double PrecioVenta { get; set; }
+        public int Stock { get; set; }
+        public int IdUsuario { get; set; }
+    }
+}
---
+++ Pro/Models/Usuarios.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pro.Models
+{
+    class Usuarios
+    {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contraseña { get; set; }
+        public string Mail { get; set; }
+    }
+}
